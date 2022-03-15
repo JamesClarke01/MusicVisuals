16322df283f8991a7e29e925a38802a49f8922a3
JamesClarke01 <81041174+JamesClarke01@users.noreply.github.com>
2022-03-15 13:16:19 +0000

    Chages

--- java/src/C20375736/Audio1.java
+++ java/src/C20375736/Audio1.java
@@ -1,6 +1,8 @@
 package C20375736;
 
 
+import java.util.concurrent.TimeUnit;
+
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
@@ -8,8 +10,8 @@ import processing.core.PApplet;
 public class Audio1 extends PApplet
 {
     Minim minim;
-    AudioPlayer ap;
-    AudioPlayer ap1;
+    AudioPlayer ambiAp;
+    AudioPlayer drumAp;
 
 
     public void settings()
@@ -21,11 +23,72 @@ public class Audio1 extends PApplet
     {
         minim = new Minim(this);
   
-        ap = minim.loadFile("ambiTestTrack.mp3", 1024);
-        ap1 = minim.loadFile("drumTestTrack.mp3", 1024);
-        ap.play();
-        ap1.play();
-      
+        ambiAp = minim.loadFile("ambiTestTrack.mp3", 1024);
+        drumAp = minim.loadFile("drumTestTrack.mp3", 1024);
+        
+        ambiAp.play();
+        drumAp.play();
+
+        //drumAp.setGain(0);
+        
+        //float startVolume = 1;
+        //float endVolume = 0f;
+
+        //drumAp.shiftVolume(startVolume, endVolume, 5);
+
+        
+
+        // while(true)
+        // {
+        //     gain--;
+        //     
+        //     System.out.println(gain);
+        // }
+        //drumAp.setGain(-50f);
+
+        // while(true)
+        // {
+        //     gain--;
+        //     drumAp.setGain(gain);
+        //     try {
+        //         TimeUnit.SECONDS.sleep(1);
+        //     } catch (InterruptedException e) {
+        //         // TODO Auto-generated catch block
+        //         e.printStackTrace();
+        //     }
+        //     System.out.println(gain);
+        // }
+        //drumAp.mute();
+        float gain;
+
+        // for(int i=0; i<100; i++)
+        // {
+
+        //     try
+        //     {
+                
+        //         TimeUnit.MILLISECONDS.sleep(500);
+        //     }
+        //     catch (InterruptedException e) {
+        //         // TODO Auto-generated catch block
+        //         e.printStackTrace();
+        //     }
+
+        //     gain = map(i, 0, 100, -25, 0);
+        //     drumAp.setGain(gain);
+            
+        //     System.out.println("i:"+i + " Gain:"+gain);
+
+        // }
+
+
+        while(true)
+        {
+            drumAp.setGain(-10);
+        }
+
+        //drumAp.setGain(-25);
+        
     }
 
 }
