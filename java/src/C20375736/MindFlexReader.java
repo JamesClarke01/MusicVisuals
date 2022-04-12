cbb243aa3fa0dbd88ec8fea4f52d810e83f2b0d4
JamesClarke01 <81041174+JamesClarke01@users.noreply.github.com>
2022-04-12 12:19:26 +0100

    jSerial package error

---
+++ java/src/C20375736/MindFlexReader.java
@@ -0,0 +1,149 @@
+package C20375736;
+
+public class MindFlexReader {
+    
+    
+    private static final int MAX_PACKET_LENGTH = 32;
+    private static final int EEG_POWER_BANDS = 8;
+
+	private static boolean inPacket = false;
+    private static int packetIndex = 0;
+    private static int packetLength = 0;
+
+    private static int checksumAccumulator = 0;
+    private static int[] packetData = new int[MAX_PACKET_LENGTH];
+    private static int[] eegPower = new int[EEG_POWER_BANDS];
+    private static int lastByte = -1;
+	
+	public static int attention = -1;
+	public static int signalQuality = 0;
+
+
+    private static void clearEegPower()
+	{
+		for (int i = 0; i < EEG_POWER_BANDS; i++)
+		{
+			eegPower[i] = 0;
+		}
+	}
+
+    private static boolean parsePacket(int[] packetData)
+	{
+		boolean parseSuccess = true;
+
+		clearEegPower(); //will reset the "power band" values  
+
+		int i = 0;
+		while( i< packetLength)
+		{
+			int bCmd = packetData[i];
+			if (bCmd==0x2)
+            {
+                signalQuality = packetData[++i];
+            }
+			else if (bCmd==0x4)
+            {
+                attention = packetData[++i];
+            }
+			else if (bCmd == 0x5)
+            {
+                //meditation value unused so skip and increment i
+                i++;
+            }
+			else if (bCmd == 0x83)
+			{
+				i++;
+
+				// Extract the values
+				for (int j = 0; j < EEG_POWER_BANDS; j++)
+				{
+					eegPower[j] = ((int)packetData[++i] << 16) | ((int)packetData[++i] << 8) | (int)packetData[++i];
+				}
+					
+			}
+			else if (bCmd == 0x80)
+			{
+				//dont use value so skip and increment i
+				i++;
+			}
+			else
+			{
+				//Broken packet
+				parseSuccess = false;
+			}
+			i++;
+		}
+		return parseSuccess;
+	}
+
+
+    public static boolean AddByte(int latestByte)
+	{//process the new received byte and return true if whole data line received
+
+		boolean bNewData = false;
+
+		//Begin building a packet
+		if (inPacket)
+		{
+			//First byte tells us the length of the upcoming packet
+			if (packetIndex == 0)
+			{
+				packetLength = latestByte;
+
+				//Throw error if packet is too long
+				if (packetLength > MAX_PACKET_LENGTH)
+				{
+					System.out.println("ERROR: Packet too long " + packetLength);
+					inPacket = false;
+				}
+			}
+			else if (packetIndex <= packetLength)
+			{
+				//Store the byte in an array
+				packetData[packetIndex - 1] = latestByte;
+				
+				//Increment the checksum
+				checksumAccumulator += latestByte;
+			}
+			else //if (packetIndex > packetLength)
+			{
+				//When we reach the end of the packet
+                
+				int checksum = latestByte;
+				
+				//note: typcasting from int to byte in java gives a range of -128 to 127
+				checksumAccumulator = (byte)(255 - checksumAccumulator);  
+				checksum = (byte)(checksum);
+			
+				//Compare checksum value
+				if (checksum == checksumAccumulator)
+				{
+					bNewData = parsePacket(packetData);
+				}
+				else
+				{
+					System.out.println("ERROR: Checksum");
+				}
+
+				//Reset and prepare for the next packet
+				inPacket = false;
+			}
+			packetIndex++;
+		}
+
+		//Look for the start of packet
+		if ((latestByte == 170) && (lastByte == 170) && !inPacket)
+		{
+			// Start of packet
+			inPacket = true;
+			packetIndex = 0;
+			checksumAccumulator = 0;
+		}
+
+		// Keep track of the last byte so we can find the sync byte pairs.
+		lastByte = latestByte;
+		return bNewData;
+	}
+
+   
+}
--- java/src/C20375736/Visualisation.java
+++ java/src/C20375736/Visualisation.java
@@ -1,6 +1,10 @@
 package C20375736;
 
+import com.fazecast.jSerialComm.*;
+
 import processing.core.PApplet;
+
+
 
 //audio stuff imports
 import ddf.minim.AudioBuffer;
@@ -17,6 +21,20 @@ public class Visualisation extends PApplet{
     AudioBuffer ambiBuffer;
 	AudioPlayer drumPlayer;
 	AudioBuffer drumBuffer;
+    
+    int mode = 0;
+
+    public void keyPressed() {
+        if (key == '1')  //mouse mode
+        {
+            mode = 1;
+        }
+        else if (key == '2')  //brain mode
+        {
+            mode = 2;
+        }
+    }
+    
     public void settings()
     {
         size(1024, 500, P3D);
@@ -40,24 +58,66 @@ public class Visualisation extends PApplet{
 
     }
 
-	public float ambiGain;
+
 	public float drumGain;
     public void draw()
     {
-		
-		//gain calc
-		if(mouseX <= width/2)
-		{
-			ambiGain = map(mouseX,0,width/2,-10,20);
-			ambiPlayer.shiftGain(ambiPlayer.getGain(),ambiGain,200);
-			drumPlayer.setGain(-200);
-		}
-		if(mouseX > width/2)
-		{
-			drumGain = map(mouseX, width/2 + 1, width,-10, 40);
-			drumPlayer.shiftGain(drumPlayer.getGain(),drumGain,200);
-			ambiPlayer.setGain(20);
-		}
+       
+ 
+		switch (mode)
+        {
+            case 1:  //mouse mode
+            {
+                drumGain = map(mouseY, height, 0,-20, 15);   //change the volume of drumtrack based on mouseY, -20 to 15 is a good range for drum volumes
+                drumPlayer.shiftGain(drumPlayer.getGain(),drumGain,200);  
+                break;  //break must be here
+            }
+            case 2:
+            {
+                System.out.println("Start");
+
+                SerialPort comPort = SerialPort.getCommPorts()[2];  
+                
+                comPort.openPort();
+
+                while (true)  //continuous loop (will loop after every byte buffer is looped through)
+                {
+                    try
+                    {
+                        byte[] byteBuffer = new byte[comPort.bytesAvailable()];  //create a buffer of length of bytes available
+                        
+                        comPort.readBytes(byteBuffer, byteBuffer.length);  //read bytes into buffer
+
+                        for(int i = 0; i < byteBuffer.length; i++)  //loop through byte buffer
+                        {
+                            byte b = byteBuffer[i];
+
+                            boolean bGoodData = MindFlexReader.AddByte(Byte.toUnsignedInt(b)); //will return true when a complete packet has been processed
+
+
+                            if (bGoodData)
+                            {
+                                System.out.println("Sig: "+ MindFlexReader.signalQuality + " Att: " + MindFlexReader.attention); 
+                
+                            }
+                        }
+                        
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("No Signal!");
+                        Thread.sleep(5); //wait
+                    }
+
+                }
+            }
+            default:
+            {
+                drumPlayer.setGain(-30);
+            }
+            
+            
+        }
 
         landscape.render(ambiBuffer);
     }
