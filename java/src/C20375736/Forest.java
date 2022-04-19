4b06179e60e7dc75259d7059cb5b53e4bc1a6ce6
James Clarke <jamesclarkedublin@gmail.com>
2022-04-19 21:46:52 +0100

    Refactored again (created Forest class)

---
+++ java/src/C20375736/Forest.java
@@ -0,0 +1,87 @@
+package C20375736;
+
+import processing.core.PApplet;
+
+import ddf.minim.AudioBuffer;
+
+public class Forest
+{
+    
+    PApplet pa;
+    Tree treeArray[];
+    final int TREEAMOUNT = 20; 
+
+    public Forest(PApplet pa)
+    {
+        this.pa = pa;
+
+        initTreeArray(TREEAMOUNT);
+    }
+
+    public void render(AudioBuffer ambiBuffer)
+    {
+        drawTrees(ambiBuffer);
+    }
+
+
+    public void initTreeArray(int treeAmount)  //call this in constructor
+    {  //calculates tree positions and populates the treeArray with tree objects
+
+        treeArray = new Tree[treeAmount]; //initialise array
+
+        float increment = (float)(pa.width/(treeAmount-1)); //treeAmount must be decremented to account for tree at first pos and end pos
+
+        float treePosX = 0;
+        Tree newTree;
+        float treeSize;
+
+        for(int i = 0; i < treeAmount; i++)
+        {
+            treeSize = pa.random(100, 150);
+            newTree = new Tree(pa, treeSize, treePosX, 0, 0);  //tree x and y left blank for now
+            treeArray[i] = newTree; 
+            treePosX += increment;
+        }	
+    }
+
+
+    public void drawTrees(AudioBuffer ambiBuffer)
+    {
+        float singleTreeSum;
+        float singleTreeAverage;
+        
+        pa.pushMatrix();
+
+        pa.translate(-(pa.width/2),0);  //set the current drawing pos to the far left of the screen
+
+        int increment = (int)(ambiBuffer.size() / TREEAMOUNT);  //potential rounding error?
+        
+        int k=0;
+
+        for(int i = 0; i < TREEAMOUNT; i++)
+        {
+            singleTreeSum = 0;
+            singleTreeAverage = 0;
+
+            //getting average of audio buffer
+            for(int j = 0; j< increment; j++)  
+            {
+                singleTreeSum += ambiBuffer.get(k);
+
+                k++;
+            }
+            singleTreeAverage = singleTreeSum/TREEAMOUNT;
+
+            //this line needs some tweaking
+            treeArray[i].setSphereRadius(pa.lerp(treeArray[i].getSphereRadius(), pa.map(singleTreeAverage, -1, 1, 0, 50) ,  0.1f));
+        }
+
+        for(int i = 0; i < treeArray.length; i++)
+        {
+            treeArray[i].render();
+        }
+
+        pa.popMatrix();
+    }
+}
+
--- java/src/C20375736/Landscape.java
+++ java/src/C20375736/Landscape.java
@@ -12,15 +12,16 @@ public class Landscape
 
     PApplet pa;
     
-    final int TREEAMOUNT = 20; 
-	Cloud cloud;	    
-    Tree treeArray[];
+	Cloud cloud;
+    Forest forest;	    
+    
 
     public Landscape(PApplet pa)
     {
         this.pa = pa;
-        initTreeArray(TREEAMOUNT);
-        cloud = new Cloud(pa);	
+        
+        cloud = new Cloud(pa);
+        forest = new Forest(pa);
     }
     
     public void render(AudioBuffer ambiBuffer, float bassModifier)
@@ -31,51 +32,14 @@ public class Landscape
 
         drawBackground();  //draw sky and ground
 
-        drawTrees(ambiBuffer);
+        forest.render(ambiBuffer);
 
 		cloud.render(bassModifier);
         
         pa.popMatrix();  //restore matrix to default
     }
 
-    public void drawTrees(AudioBuffer ambiBuffer)
-    {
-        float singleTreeSum;
-        float singleTreeAverage;
-        
-        pa.pushMatrix();
-
-        pa.translate(-(pa.width/2),0);  //set the current drawing pos to the far left of the screen
-
-        int increment = (int)(ambiBuffer.size() / TREEAMOUNT);  //potential rounding error?
-        
-        int k=0;
-
-        for(int i = 0; i < TREEAMOUNT; i++)
-        {
-            singleTreeSum = 0;
-            singleTreeAverage = 0;
-
-            //getting average of audio buffer
-            for(int j = 0; j< increment; j++)  
-            {
-                singleTreeSum += ambiBuffer.get(k);
-
-                k++;
-            }
-            singleTreeAverage = singleTreeSum/TREEAMOUNT;
-
-            //this line needs some tweaking
-            treeArray[i].setSphereRadius(pa.lerp(treeArray[i].getSphereRadius(), pa.map(singleTreeAverage, -1, 1, 0, 50) ,  0.1f));
-        }
-
-        for(int i = 0; i < treeArray.length; i++)
-        {
-            treeArray[i].render();
-        }
-
-        pa.popMatrix();
-    }
+    
 
 
     public void drawBackground()
@@ -87,25 +51,7 @@ public class Landscape
         pa.box(pa.width*2, 1, 750); //draws ground (hard coded values need to be removed)
     }
 
-    public void initTreeArray(int treeAmount)  //call this in constructor
-    {  //calculates tree positions and populates the treeArray with tree objects
-
-        treeArray = new Tree[treeAmount]; //initialise array
-
-        float increment = (float)(pa.width/(treeAmount-1)); //treeAmount must be decremented to account for tree at first pos and end pos
-
-        float treePosX = 0;
-        Tree newTree;
-        float treeSize;
-
-        for(int i = 0; i < treeAmount; i++)
-        {
-            treeSize = pa.random(100, 150);
-            newTree = new Tree(pa, treeSize, treePosX, 0, 0);  //tree x and y left blank for now
-            treeArray[i] = newTree; 
-            treePosX += increment;
-        }	
-    }
+    
 	
 
 }
