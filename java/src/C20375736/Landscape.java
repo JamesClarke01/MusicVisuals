4c86e982e44be09deefe003672eecf156ffa35e4
James Clarke <jamesclarkedublin@gmail.com>
2022-05-02 18:01:45 +0100

    LandscapeDarkening

--- java/src/C20375736/Landscape.java
+++ java/src/C20375736/Landscape.java
@@ -23,30 +23,38 @@ public class Landscape
         forest = new Forest(pa);
     }
     
-    public void render(AudioBuffer ambiBuffer, AudioBuffer bassBuffer,float bassModifier)
+    public void render(AudioBuffer ambiBuffer, AudioBuffer bassBuffer, float bassModifier)
     {
         pa.pushMatrix();
 
         pa.translate(pa.width/2, pa.height); //translate to bottom
 
-        drawBackground();  //draw sky and ground
+        drawBackground(bassModifier);  //draw sky and ground
 
         forest.render(ambiBuffer);
 
 		cloud.render(bassBuffer,bassModifier);
         
-
         pa.popMatrix();  //restore matrix to default
     }
 
     
-    public void drawBackground()
+    public void drawBackground(float bassModifier)
     {
-        pa.background(28, 221, 255);
+        float BACK_MODIFIER = pa.map(bassModifier, 0, 100, 255, 80);
+        float GROUND_MODIFIER = pa.map(bassModifier, 0, 100, 255, 130);
+
+
+        pa.colorMode(pa.HSB);
         
-        pa.fill(56, 232, 53);  //ground colour
+        pa.background(135, BACK_MODIFIER, BACK_MODIFIER);  //rgb: 28, 221, 255
+        
+        pa.fill(90, GROUND_MODIFIER, GROUND_MODIFIER);  //ground colour, rgb:  56, 232, 53
+        
         
         pa.box(pa.width*2, 1, 750); //draws ground (hard coded values need to be removed)
+
+        pa.colorMode(pa.RGB);
     }
 
 }
