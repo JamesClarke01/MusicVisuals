248356300a47e077d5c7c3ec8da3d22b191d3af1
davidlynch-sd <davite3@gmail.com>
2022-05-03 22:29:07 +0100

    Random Lightning

--- java/src/C20375736/Volley.java
+++ java/src/C20375736/Volley.java
@@ -27,7 +27,7 @@ public class Volley
 		Bolt newBolt;
 		for(int i = 0; i < BOLTAMOUNT; i++)
 		{
-			newBolt = new Bolt(pa,boltX + (gap * i) - pa.width/2,-600,0);
+			newBolt = new Bolt(pa,boltX + (gap * i) - pa.width/2,-500,-100);
 			boltArray[i] = newBolt;
 		}
 	}
@@ -37,7 +37,7 @@ public class Volley
 		for(int i = 0; i < BOLTAMOUNT; i++)
 		{
 			boltArray[i].charge();
-			boltArray[i].strike((int)pa.map(drumBuffer.level(),0,0.1f,0,12),40, drumModifier);
+			boltArray[i].strike((int)pa.map(drumBuffer.level(),0,0.1f,0,12) - (int)pa.random(0,6),40, drumModifier);
 			boltArray[i].discharge();
 		}
 	}
